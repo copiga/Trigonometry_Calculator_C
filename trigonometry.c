235a2a60bfd1e3f7983b25d2c87d5ddda0961e76
Josh <ben11107138@coleg-powys.ac.uk>
2012-06-14 14:54:23 +0200

    added a selector for stuff...

--- trigonometry.c
+++ trigonometry.c
@@ -1,19 +1,34 @@
-/***************************************
-**There is nothing here yet, so using **
-**any trigonometry beyond pythagoras  **
+/*******************************************
+**There is nothing here yet, so using     **
+**any trigonometry beyond pythagoras      **
 **in this programme is quite impossible...**
-***************************************/
+*******************************************/
 #include <stdio.h>
 #include <math.h>
 
 trigonometry (void){
+	char selector;
+	printf ("do you have lines and angles, just angles or just lines? \nFor just angles please enter "aa" for just lines please enter "ll" and for a mix of the two please enter "la");
+	scanf ("%s",selector);
+	if (selector == "aa" || selector == "AA"){
+		angleangle();
+	}else if (selector == "ll" || selector == "LL"){
+		lineline();
+	}else if (selector == "la" || selector == "al" || selector == "LA" || selector == "AL"){
+		angleline();
+	}else{
+		main();
+	}
+}
+
+angleangle (void){
 	
 }
 
-angle (void){
-	
+angleline (void){
+
 }
 
-line(void){
+lineline (void){
 	double a,b,c,A,B,C;
 }
